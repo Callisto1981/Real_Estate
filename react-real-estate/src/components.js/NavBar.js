c3bb630d4459d2756f790fae6609cb7f60f7f351
Wayne <wabbruscatoorr@gmail.com>
2021-06-27 18:32:29 -0400

    Updating files

--- react-real-estate/src/components.js/NavBar.js
+++ react-real-estate/src/components.js/NavBar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const Nav = styled.nav`
+    height: 60px;
+    background: blue;
+`;
+
+const Logo = styled(Link)`
+    color: #fff;
+`;
+
+const MenuBars =styled.i``;
+
+const NavMenu = styled.div``;
 
 export const NavBar = () => {
     return (
-        <div>
+        <Nav>
+            <Logo>ELIXR</Logo>
+            <MenuBars/>
+            <NavMenu>
+
+            </NavMenu>
             <h1>NavBar</h1>
-        </div>
+        </Nav>
     )
 }
